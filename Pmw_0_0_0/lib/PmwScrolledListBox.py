e2a5a4e88ca5a2ad88dfe3feae494e3e99e240e8
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2009-07-07 00:35:21 -0400

    fix indentation to be consistent 4 spaces

--- Pmw_0_0_0/lib/PmwScrolledListBox.py
+++ Pmw_0_0_0/lib/PmwScrolledListBox.py
@@ -9,64 +9,62 @@ class ScrolledListBox(Pmw.MegaWidget):
 
     def __init__(self, parent = None, **kw):
 
-	# Define the megawidget options.
-	INITOPT = Pmw.INITOPT
-	optiondefs = (
-	    ('dblclickcommand',    None,            None),
-	    ('hscrollmode',        'dynamic',       self._hscrollMode),
-	    ('items',              (),              INITOPT),
-	    ('labelmargin',        0,               INITOPT),
-	    ('labelpos',           None,            INITOPT),
-	    ('scrollmargin',       2,               INITOPT),
-	    ('selectioncommand',   None,            None),
-	    ('usehullsize',        0,               INITOPT),
-	    ('vscrollmode',        'dynamic',       self._vscrollMode),
-	)
-	self.defineoptions(kw, optiondefs)
+        # Define the megawidget options.
+        INITOPT = Pmw.INITOPT
+        optiondefs = (
+            ('dblclickcommand',    None,            None),
+            ('hscrollmode',        'dynamic',       self._hscrollMode),
+            ('items',              (),              INITOPT),
+            ('labelmargin',        0,               INITOPT),
+            ('labelpos',           None,            INITOPT),
+            ('scrollmargin',       2,               INITOPT),
+            ('selectioncommand',   None,            None),
+            ('usehullsize',        0,               INITOPT),
+            ('vscrollmode',        'dynamic',       self._vscrollMode),
+        )
+        self.defineoptions(kw, optiondefs)
 
-	# Initialise the base class (after defining the options).
-	Pmw.MegaWidget.__init__(self, parent)
+        # Initialise the base class (after defining the options).
+        Pmw.MegaWidget.__init__(self, parent)
 
-	# Create the components.
-	interior = self.interior()
+        # Create the components.
+        interior = self.interior()
 
-	if self['usehullsize']:
-	    interior.grid_propagate(0)
+        if self['usehullsize']:
+            interior.grid_propagate(0)
 
-	# Create the listbox widget.
-	self._listbox = self.createcomponent('listbox',
-		(), None,
-		Tkinter.Listbox, (interior,))
-	self._listbox.grid(row = 2, column = 2, sticky = 'news')
-	interior.grid_rowconfigure(2, weight = 1, minsize = 0)
-	interior.grid_columnconfigure(2, weight = 1, minsize = 0)
+        # Create the listbox widget.
+        self._listbox = self.createcomponent('listbox',
+            (), None,
+            Tkinter.Listbox, (interior,))
+        self._listbox.grid(row = 2, column = 2, sticky = 'news')
+        interior.grid_rowconfigure(2, weight = 1, minsize = 0)
+        interior.grid_columnconfigure(2, weight = 1, minsize = 0)
 
-	# Create the horizontal scrollbar
-	self._horizScrollbar = self.createcomponent('horizscrollbar',
-		(), 'Scrollbar',
-		Tkinter.Scrollbar, (interior,),
-	        orient='horizontal',
-		command=self._listbox.xview
-	)
+        # Create the horizontal scrollbar
+        self._horizScrollbar = self.createcomponent('horizscrollbar',
+            (), 'Scrollbar',
+            Tkinter.Scrollbar, (interior,),
+            orient='horizontal',
+            command=self._listbox.xview)
 
-	# Create the vertical scrollbar
-	self._vertScrollbar = self.createcomponent('vertscrollbar',
-		(), 'Scrollbar',
-		Tkinter.Scrollbar, (interior,),
-		orient='vertical',
-		command=self._listbox.yview
-	)
+        # Create the vertical scrollbar
+        self._vertScrollbar = self.createcomponent('vertscrollbar',
+            (), 'Scrollbar',
+            Tkinter.Scrollbar, (interior,),
+            orient='vertical',
+            command=self._listbox.yview)
 
-	self.createlabel(interior, childCols = 3, childRows = 3)
+        self.createlabel(interior, childCols = 3, childRows = 3)
 
-	# Add the items specified by the initialisation option.
-	items = self['items']
-	if type(items) != types.TupleType:
-	    items = tuple(items)
-	if len(items) > 0:
-	    apply(self._listbox.insert, ('end',) + items)
+        # Add the items specified by the initialisation option.
+        items = self['items']
+        if type(items) != types.TupleType:
+            items = tuple(items)
+        if len(items) > 0:
+            apply(self._listbox.insert, ('end',) + items)
 
-	_registerScrolledList(self._listbox, self)
+        _registerScrolledList(self._listbox, self)
 
         # Establish the special class bindings if not already done.
         # Also create bindings if the Tkinter default interpreter has
@@ -77,7 +75,7 @@ class ScrolledListBox(Pmw.MegaWidget):
         theTag = 'ScrolledListBoxTag'
         if ScrolledListBox._classBindingsDefinedFor != Tkinter._default_root:
             root  = Tkinter._default_root
-	    	    
+            
             def doubleEvent(event):
                 _handleEvent(event, 'double')
             def keyEvent(event):
@@ -93,41 +91,41 @@ class ScrolledListBox(Pmw.MegaWidget):
             # Bind double button 1 click to the dblclickcommand.
             root.bind_class(theTag, '<Double-ButtonRelease-1>', doubleEvent)
 
-	    ScrolledListBox._classBindingsDefinedFor = root
+        ScrolledListBox._classBindingsDefinedFor = root
 
-	bindtags = self._listbox.bindtags()
-	self._listbox.bindtags(bindtags + (theTag,))
+        bindtags = self._listbox.bindtags()
+        self._listbox.bindtags(bindtags + (theTag,))
 
-	# Initialise instance variables.
-	self._horizScrollbarOn = 0
-	self._vertScrollbarOn = 0
-	self.scrollTimer = None
+        # Initialise instance variables.
+        self._horizScrollbarOn = 0
+        self._vertScrollbarOn = 0
+        self.scrollTimer = None
         self._scrollRecurse = 0
-	self._horizScrollbarNeeded = 0
-	self._vertScrollbarNeeded = 0
+        self._horizScrollbarNeeded = 0
+        self._vertScrollbarNeeded = 0
 
-	# Check keywords and initialise options.
-	self.initialiseoptions()
+        # Check keywords and initialise options.
+        self.initialiseoptions()
 
     def destroy(self):
-	if self.scrollTimer is not None:
-	    self.after_cancel(self.scrollTimer)
-	    self.scrollTimer = None
-	_deregisterScrolledList(self._listbox)
-	Pmw.MegaWidget.destroy(self)
+        if self.scrollTimer is not None:
+            self.after_cancel(self.scrollTimer)
+            self.scrollTimer = None
+        _deregisterScrolledList(self._listbox)
+        Pmw.MegaWidget.destroy(self)
 
     # ======================================================================
 
     # Public methods.
 
     def clear(self):
-	self.setlist(())
+        self.setlist(())
 
     def getcurselection(self):
-	rtn = []
-	for sel in self.curselection():
-	    rtn.append(self._listbox.get(sel))
-	return tuple(rtn)
+        rtn = []
+        for sel in self.curselection():
+            rtn.append(self._listbox.get(sel))
+        return tuple(rtn)
 
     def getvalue(self):
         return self.getcurselection()
@@ -149,60 +147,60 @@ class ScrolledListBox(Pmw.MegaWidget):
 
     def setlist(self, items):
         self._listbox.delete(0, 'end')
-	if len(items) > 0:
-	    if type(items) != types.TupleType:
-		items = tuple(items)
-	    apply(self._listbox.insert, (0,) + items)
+        if len(items) > 0:
+            if type(items) != types.TupleType:
+                items = tuple(items)
+                apply(self._listbox.insert, (0,) + items)
 
     # Override Tkinter.Listbox get method, so that if it is called with
     # no arguments, return all list elements (consistent with other widgets).
     def get(self, first=None, last=None):
-	if first is None:
-	    return self._listbox.get(0, 'end')
-	else:
-	    return self._listbox.get(first, last)
+        if first is None:
+            return self._listbox.get(0, 'end')
+        else:
+            return self._listbox.get(first, last)
 
     # ======================================================================
 
     # Configuration methods.
 
     def _hscrollMode(self):
-	# The horizontal scroll mode has been configured.
+        '''The horizontal scroll mode has been configured.'''
 
-	mode = self['hscrollmode']
+        mode = self['hscrollmode']
 
-	if mode == 'static':
-	    if not self._horizScrollbarOn:
-		self._toggleHorizScrollbar()
-	elif mode == 'dynamic':
-	    if self._horizScrollbarNeeded != self._horizScrollbarOn:
-		self._toggleHorizScrollbar()
-	elif mode == 'none':
-	    if self._horizScrollbarOn:
-		self._toggleHorizScrollbar()
-	else:
-	    message = 'bad hscrollmode option "%s": should be static, dynamic, or none' % mode
-	    raise ValueError, message
+        if mode == 'static':
+            if not self._horizScrollbarOn:
+                self._toggleHorizScrollbar()
+        elif mode == 'dynamic':
+            if self._horizScrollbarNeeded != self._horizScrollbarOn:
+                self._toggleHorizScrollbar()
+        elif mode == 'none':
+            if self._horizScrollbarOn:
+                self._toggleHorizScrollbar()
+        else:
+            message = 'bad hscrollmode option "%s": should be static, dynamic, or none' % mode
+            raise ValueError, message
 
         self._configureScrollCommands()
 
     def _vscrollMode(self):
-	# The vertical scroll mode has been configured.
+        '''The vertical scroll mode has been configured.'''
 
-	mode = self['vscrollmode']
+        mode = self['vscrollmode']
 
-	if mode == 'static':
-	    if not self._vertScrollbarOn:
-		self._toggleVertScrollbar()
-	elif mode == 'dynamic':
-	    if self._vertScrollbarNeeded != self._vertScrollbarOn:
-		self._toggleVertScrollbar()
-	elif mode == 'none':
-	    if self._vertScrollbarOn:
-		self._toggleVertScrollbar()
-	else:
-	    message = 'bad vscrollmode option "%s": should be static, dynamic, or none' % mode
-	    raise ValueError, message
+        if mode == 'static':
+            if not self._vertScrollbarOn:
+                self._toggleVertScrollbar()
+        elif mode == 'dynamic':
+            if self._vertScrollbarNeeded != self._vertScrollbarOn:
+                self._toggleVertScrollbar()
+        elif mode == 'none':
+            if self._vertScrollbarOn:
+                self._toggleVertScrollbar()
+        else:
+            message = 'bad vscrollmode option "%s": should be static, dynamic, or none' % mode
+            raise ValueError, message
 
         self._configureScrollCommands()
 
@@ -223,7 +221,7 @@ class ScrolledListBox(Pmw.MegaWidget):
         if tclCommandName != '':   
             self._listbox.deletecommand(tclCommandName)
 
-	if self['hscrollmode'] == self['vscrollmode'] == 'dynamic':
+        if self['hscrollmode'] == self['vscrollmode'] == 'dynamic':
             self._listbox.configure(
                     xscrollcommand=self._scrollBothLater,
                     yscrollcommand=self._scrollBothLater
@@ -238,9 +236,9 @@ class ScrolledListBox(Pmw.MegaWidget):
         self._horizScrollbar.set(first, last)
         self._horizScrollbarNeeded = ((first, last) != ('0', '1'))
 
-	if self['hscrollmode'] == 'dynamic':
-	    if self._horizScrollbarNeeded != self._horizScrollbarOn:
-		self._toggleHorizScrollbar()
+        if self['hscrollmode'] == 'dynamic':
+            if self._horizScrollbarNeeded != self._horizScrollbarOn:
+                self._toggleHorizScrollbar()
 
     def _scrollYNow(self, first, last):
         self._vertScrollbar.set(first, last)
@@ -251,16 +249,19 @@ class ScrolledListBox(Pmw.MegaWidget):
                 self._toggleVertScrollbar()
 
     def _scrollBothLater(self, first, last):
-	# Called by the listbox to set the horizontal or vertical
-	# scrollbar when it has scrolled or changed size or contents.
+        '''Called by the listbox to set the horizontal or vertical
+        scrollbar when it has scrolled or changed size or contents.
+        '''
 
-	if self.scrollTimer is None:
-	    self.scrollTimer = self.after_idle(self._scrollBothNow)
+        if self.scrollTimer is None:
+            self.scrollTimer = self.after_idle(self._scrollBothNow)
 
     def _scrollBothNow(self):
-        # This performs the function of _scrollXNow and _scrollYNow.
-        # If one is changed, the other should be updated to match.
-	self.scrollTimer = None
+        '''This performs the function of _scrollXNow and _scrollYNow.
+        
+        If one is changed, the other should be updated to match.
+        '''
+        self.scrollTimer = None
 
         # Call update_idletasks to make sure that the containing frame
         # has been resized before we attempt to set the scrollbars. 
@@ -271,59 +272,59 @@ class ScrolledListBox(Pmw.MegaWidget):
         if self._scrollRecurse != 0:
             return
 
-	xview = self._listbox.xview()
-	yview = self._listbox.yview()
-	self._horizScrollbar.set(xview[0], xview[1])
-	self._vertScrollbar.set(yview[0], yview[1])
+        xview = self._listbox.xview()
+        yview = self._listbox.yview()
+        self._horizScrollbar.set(xview[0], xview[1])
+        self._vertScrollbar.set(yview[0], yview[1])
 
-	self._horizScrollbarNeeded = (xview != (0.0, 1.0))
-	self._vertScrollbarNeeded = (yview != (0.0, 1.0))
+        self._horizScrollbarNeeded = (xview != (0.0, 1.0))
+        self._vertScrollbarNeeded = (yview != (0.0, 1.0))
 
-	# If both horizontal and vertical scrollmodes are dynamic and
-	# currently only one scrollbar is mapped and both should be
-	# toggled, then unmap the mapped scrollbar.  This prevents a
-	# continuous mapping and unmapping of the scrollbars. 
-	if (self['hscrollmode'] == self['vscrollmode'] == 'dynamic' and
-		self._horizScrollbarNeeded != self._horizScrollbarOn and
-		self._vertScrollbarNeeded != self._vertScrollbarOn and
-		self._vertScrollbarOn != self._horizScrollbarOn):
-	    if self._horizScrollbarOn:
-		self._toggleHorizScrollbar()
-	    else:
-		self._toggleVertScrollbar()
-	    return
+        # If both horizontal and vertical scrollmodes are dynamic and
+        # currently only one scrollbar is mapped and both should be
+        # toggled, then unmap the mapped scrollbar.  This prevents a
+        # continuous mapping and unmapping of the scrollbars. 
+        if (self['hscrollmode'] == self['vscrollmode'] == 'dynamic' and
+            self._horizScrollbarNeeded != self._horizScrollbarOn and
+            self._vertScrollbarNeeded != self._vertScrollbarOn and
+            self._vertScrollbarOn != self._horizScrollbarOn):
+            if self._horizScrollbarOn:
+                self._toggleHorizScrollbar()
+            else:
+                self._toggleVertScrollbar()
+            return
 
-	if self['hscrollmode'] == 'dynamic':
-	    if self._horizScrollbarNeeded != self._horizScrollbarOn:
-		self._toggleHorizScrollbar()
+        if self['hscrollmode'] == 'dynamic':
+            if self._horizScrollbarNeeded != self._horizScrollbarOn:
+                self._toggleHorizScrollbar()
 
-	if self['vscrollmode'] == 'dynamic':
-	    if self._vertScrollbarNeeded != self._vertScrollbarOn:
-		self._toggleVertScrollbar()
+        if self['vscrollmode'] == 'dynamic':
+            if self._vertScrollbarNeeded != self._vertScrollbarOn:
+                self._toggleVertScrollbar()
 
     def _toggleHorizScrollbar(self):
 
-	self._horizScrollbarOn = not self._horizScrollbarOn
+        self._horizScrollbarOn = not self._horizScrollbarOn
 
-	interior = self.interior()
-	if self._horizScrollbarOn:
-	    self._horizScrollbar.grid(row = 4, column = 2, sticky = 'news')
-	    interior.grid_rowconfigure(3, minsize = self['scrollmargin'])
-	else:
-	    self._horizScrollbar.grid_forget()
-	    interior.grid_rowconfigure(3, minsize = 0)
+        interior = self.interior()
+        if self._horizScrollbarOn:
+            self._horizScrollbar.grid(row = 4, column = 2, sticky = 'news')
+            interior.grid_rowconfigure(3, minsize = self['scrollmargin'])
+        else:
+            self._horizScrollbar.grid_forget()
+            interior.grid_rowconfigure(3, minsize = 0)
 
     def _toggleVertScrollbar(self):
 
-	self._vertScrollbarOn = not self._vertScrollbarOn
+        self._vertScrollbarOn = not self._vertScrollbarOn
 
-	interior = self.interior()
-	if self._vertScrollbarOn:
-	    self._vertScrollbar.grid(row = 2, column = 4, sticky = 'news')
-	    interior.grid_columnconfigure(3, minsize = self['scrollmargin'])
-	else:
-	    self._vertScrollbar.grid_forget()
-	    interior.grid_columnconfigure(3, minsize = 0)
+        interior = self.interior()
+        if self._vertScrollbarOn:
+            self._vertScrollbar.grid(row = 2, column = 4, sticky = 'news')
+            interior.grid_columnconfigure(3, minsize = self['scrollmargin'])
+        else:
+            self._vertScrollbar.grid_forget()
+            interior.grid_columnconfigure(3, minsize = 0)
 
     def _handleEvent(self, event, eventType):
         if eventType == 'double':
@@ -345,12 +346,12 @@ class ScrolledListBox(Pmw.MegaWidget):
     # Need to explicitly forward this to override the stupid
     # (grid_)size method inherited from Tkinter.Frame.Grid.
     def size(self):
-	return self._listbox.size()
+        return self._listbox.size()
 
     # Need to explicitly forward this to override the stupid
     # (grid_)bbox method inherited from Tkinter.Frame.Grid.
     def bbox(self, index):
-	return self._listbox.bbox(index)
+        return self._listbox.bbox(index)
 
 Pmw.forwardmethods(ScrolledListBox, Tkinter.Listbox, '_listbox')
 
